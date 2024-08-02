8bff75f61bdd893b9d763a2c84cc2a08dd073338
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-08-02 22:05:11 +0000

    perfected the DAL

--- core/database/DALs/user/user_dal.py
+++ core/database/DALs/user/user_dal.py
@@ -9,37 +9,100 @@ from core.database.DALs.base import BaseDAL
 from core.type_hints import Email
 
 class UserDAL(BaseDAL):
-    """Data Access Layer for managing users in the database."""
-    def __init__(self, db_session: Session):
+    """
+        Data Access Layer for managing users in the database.
+
+        Attributes:
+            db_session (Session): The database session.
+    """
+    def __init__(self, db_session: Session) -> None:
         self.db_session = db_session
     
-    async def new(self, user: User):
-        """Add a new user to the database."""
+    async def new(self, user: User) -> User:
+        """
+            Add a new user to the database.
+
+            Attributes:
+                user (User): The user to add to the database.
+            
+            Returns:
+                User: The user that was added to the database.
+        """
         self.db_session.add(user)
         self.db_session.commit()
         self.db_session.refresh(user)
         return user
     
-    async def get(self, uuid: UUID) -> User:
-        """Retrieve a user from the database."""
+    async def get(self, uuid: UUID) -> User | None:
+        """
+            Retrieve a user from the database.
+
+            Attributes:
+                uuid (UUID): The UUID of the user to retrieve.
+            
+            Returns:
+                User | None: The user with the specified UUID, or None if not found
+        """
         return self.db_session.query(User).filter(User.uuid == uuid).first()
     
-    async def get_by_email(self, email: Email) -> User:
-        """Retrieve a user from the database by email."""
+    async def get_by_email(self, email: Email) -> User | None:
+        """
+            Retrieve a user from the database by email.
+
+            Attributes:
+                email (Email): The email of the user to retrieve.
+            
+            Returns:
+                User | Nonw: The user with the specified email, or None if not found.
+        """
         return self.db_session.query(User).filter(User.email == email).first()
-    
+
     async def update(self, user: User) -> User:
-        """Update a user in the database."""
+        """
+            Update a user in the database.
+
+            Attributes:
+                user (User): The user to update in the database.
+            
+            Returns:
+                User: The user that was updated in the database.
+        """
         self.db_session.commit()
         self.db_session.refresh(user)
         return user
     
+    async def refresh(self, user: User) -> User:
+        """
+            Refresh an instance of a user object from database.
+
+            Attributes:
+                user (User): The user object to refresh.
+            
+            Returns:
+                User: The user that was refreshed in the database.
+        """
+        self.db_session.refresh(user)
+        return user
+
     async def delete(self, user: User) -> User:
-        """Delete a user from the database."""
+        """
+            Delete a user from the database.
+
+            Attributes:
+                user (User): The user to delete from the database.
+            
+            Returns:
+                User: The user that was deleted from the database.
+        """
         self.db_session.delete(user)
         self.db_session.commit()
         return user
     
     async def get_all(self) -> list:
-        """Retrieve all users from the database."""
+        """
+            Retrieve all users from the database.
+
+            Returns:
+                list[User]: A list of all users in the database
+        """
         return self.db_session.query(User).all()
