e754932327338c4211cedac2dfa12773935ea33f
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-08-04 00:10:03 +0000

    changes slightly

--- api/v0/views/index.py
+++ api/v0/views/index.py
@@ -5,7 +5,7 @@ from sanic import Request
 from sanic.views import HTTPMethodView
 from sanic import text
 from sanic_ext import validate
-
+from sanic.response import HTTPResponse
 from core.database.models.user.User import User
 
 class IndexView(HTTPMethodView):
@@ -22,21 +22,21 @@ class IndexView(HTTPMethodView):
         text: str
 
     @validate(query=QueryParams)
-    async def get(self, request: Request):
+    async def get(self, request: Request) -> HTTPResponse:
         """
             Handles GET requests to the index view.
         """
         return await text('I am get method with a query param `q`')
 
     @validate(json=Json)
-    async def post(self, request: Request, user: User):
+    async def post(self, request: Request, user: User) -> HTTPResponse:
         """
             Handles POST requests to the index view.
         """
 
         return await text('I am a protected post method with json body and user object injected.')
 
-    async def put(self, request: Request):
+    async def put(self, request: Request) -> HTTPResponse:
         """
             Handles PUT requests to the index view.
         """
