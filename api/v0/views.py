5d23833d310319446c03237b92d0057a38bd6d18
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-08-07 09:06:21 +0000

    fixed linter issue

--- api/v0/views.py
+++ api/v0/views.py
@@ -1,6 +1,6 @@
 """ This file is used to import all the views from the api.v0.views module. """
 
-from api.v0.views.index import IndexView
+from api.v0.views.index import IndexView # type: ignore
 
 views = [
     IndexView,
