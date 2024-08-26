ac9425cb5ee94cf6f908f525cf29b3eaaa6b0d39
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-08-26 18:20:40 +0000

    Addition of doctrings for custom types

--- custom_types.py
+++ custom_types.py
@@ -8,12 +8,29 @@ from sqlalchemy.ext.declarative import ConcreteBase
 
 @dataclass
 class DatabaseContext:
+    """
+        Sub-custom-context for database spesific actions
+
+        Args:
+            engine: sqlalchemy.ext.asyncio.AsyncEngine
+            asyncsession: sqlalchemy.orm.sessionmaker
+            Base: sqlalchemy.ext.declarative.ConcreteBase
+    """
     engine: AsyncEngine
     asyncsession: sessionmaker
     Base: ConcreteBase
 
 @dataclass
 class MyTypedContext:
+    """
+        Custom typed context for the app.ctx object
+
+        Args:
+            db: DatabaseContext
+            env_manager: core.env_manager.EnvManager
+            session_manager: core.session_manager.SessionManager
+            cache_manager: core.cache_manager.CacheManager
+    """
     db: DatabaseContext
     env_manager: EnvManager
     session_manager: SessionManager
